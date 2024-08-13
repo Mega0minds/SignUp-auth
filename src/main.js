6aa34af041e2f1dbc3ed6427feeff2ba665c7918
Mega0minds <megaolubakin@gmail.com>
2024-08-13 12:52:01 +0100

    worked on confirm password

--- src/main.js
+++ src/main.js
@@ -4,3 +4,17 @@ document.getElementById('signup-form').addEventListener('submit', (event) => {
     localStorage.setItem('signupEmail', email);
     alert(`Email ${email} has been saved!`);
 });
+
+const password = document.getElementById('password');
+const confirm_password = document.getElementById('confirm_password');
+
+function validatePassword() {
+    if (password.value != confirm_password.value) {
+        confirm_password.setCustomValidity("Passwords dont match")
+    } else {
+        confirm_password.setCustomValidity("")
+    }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
--- vite.config.js
+++ vite.config.js
@@ -18,6 +18,7 @@ export default defineConfig({
         main: './index.html',
         login: './login.html', 
         contsinup: './SignupCont.html',
+        dashboard: './dashboard.html',
       }
     }
   }
